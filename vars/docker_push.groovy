66a0bddd972efc3816a385c23bdd31777963d2b0
nkasbe21 <nishakasbe2101@gmail.com>
2026-03-31 15:55:20 +0530

    Change shell command from sh to bat for Windows

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -10,7 +10,7 @@ def call(Map config = [:]) {
         usernameVariable: 'DOCKER_USERNAME',
         passwordVariable: 'DOCKER_PASSWORD'
     )]) {
-        sh """
+        bat """
             echo "\$DOCKER_PASSWORD" | docker login -u "\$DOCKER_USERNAME" --password-stdin
             docker push ${imageName}:${imageTag}
             docker push ${imageName}:latest
