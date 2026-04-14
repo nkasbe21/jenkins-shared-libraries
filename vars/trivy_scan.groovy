4ab04f1716c003ac4e528e98a843e747380aa593
nkasbe21 <nishakasbe2101@gmail.com>
2026-04-14 12:40:56 +0530

    Update trivy_scan.groovy

--- vars/trivy_scan.groovy
+++ vars/trivy_scan.groovy
@@ -1,3 +1,3 @@
-def call(){
-  bat "docker run --rm -v /var/run/docker.sock:/var/run/docker.sock aquasec/trivy fs ."
+def call() {
+  bat "docker run --rm -v %cd%:/app -w /app aquasec/trivy fs ."
 }
