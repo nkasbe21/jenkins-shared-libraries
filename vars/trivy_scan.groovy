06b8b42e5a3623d0378feb60eff4d34197ed6262
nkasbe21 <nishakasbe2101@gmail.com>
2026-04-14 12:47:56 +0530

    Update Trivy scan command to include exit code and severity

--- vars/trivy_scan.groovy
+++ vars/trivy_scan.groovy
@@ -1,3 +1,5 @@
 def call() {
-  bat "docker run --rm -v %cd%:/app -w /app aquasec/trivy:0 fs ."
+  bat """
+  docker run --rm -v %cd%:/app -w /app aquasec/trivy:0 fs --exit-code 1 --severity HIGH,CRITICAL .
+  """
 }
