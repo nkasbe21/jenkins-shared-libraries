2ca505ce4e73b032b5496c4e99d9dc2858dc39c7
nkasbe21 <nishakasbe2101@gmail.com>
2026-04-14 12:52:25 +0530

    Update trivy_scan.groovy

--- vars/trivy_scan.groovy
+++ vars/trivy_scan.groovy
@@ -1,5 +1,3 @@
 def call() {
-  bat """
-  docker run --rm -v %cd%:/app -w /app aquasec/trivy:0 fs --exit-code 1 --severity HIGH,CRITICAL .
-  """
+  bat "docker run --rm -v %cd%:/app -w /app aquasec/trivy:canary fs ."
 }
